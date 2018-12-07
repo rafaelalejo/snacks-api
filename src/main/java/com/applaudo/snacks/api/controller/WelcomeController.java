03b7b852dd2bab4367dfdd6023b241d002ca2ffb
Rafael Alejo <rafael_alejo@outlook.com>
2018-12-07 15:26:00 -0600

    another test, just in case

--- src/main/java/com/applaudo/snacks/api/controller/WelcomeController.java
+++ src/main/java/com/applaudo/snacks/api/controller/WelcomeController.java
@@ -5,10 +5,11 @@ import com.applaudo.snacks.api.response.CustomMessageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Test controller for CI/CD integration
 @RestController
 public class WelcomeController {
 	@GetMapping("/")
 	public CustomMessageResponse hello() {
-		return new CustomMessageResponse("Hola Applaudo?");
+		return new CustomMessageResponse("Hola Applaudo!");
 	}
 }
