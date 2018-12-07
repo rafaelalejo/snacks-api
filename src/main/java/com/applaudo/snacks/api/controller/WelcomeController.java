c873d0bf9d2fc5be39dad4bdec9322a02fdad919
Rafael Alejo <rafael_alejo@outlook.com>
2018-12-07 11:58:29 -0600

    Changed .war output name, added a welcome controller

---
+++ src/main/java/com/applaudo/snacks/api/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.applaudo.snacks.api.controller;
+
+import com.applaudo.snacks.api.response.CustomMessageResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping("/")
+	public CustomMessageResponse hello() {
+		return new CustomMessageResponse("Hola Applaudo!");
+	}
+}
